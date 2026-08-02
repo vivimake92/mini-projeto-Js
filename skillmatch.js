d8cf0346f9c260410a6006b2039d6effdaaa28cb
vivimake92 <vivi.make92@gmail.com>
2026-08-02 16:15:08 -0300

    feat: adiciona perfil do candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,28 @@
+const candidato = {
+    name: "Vivian",
+    idade: 34,
+    areaInteresse: "Desenvolvimento Frontend júnior",
+    habilidades: [
+        "JavaScript", "Git", "GitHub"
+    ],
+    TempoExperiencia: "Sem experiência profissional"
+}
+
+//console.log(candidato)
+
+
+const vagas = [
+    {"empresa": "Virtual Quest", "cargo": "Desenvolvedor Front-end júnior", "lista de requisitos" : [
+        "Javascript", "Git", "Github", "CSS", "Sem experiência profissional"
+    ]},
+
+    {"empresa": "Talent&Tech", "cargo": "Desenvolvedor Júnior", "lista de requisitos" : [
+        "SQL", "Python", "Javascript", "Html5"
+    ]},
+
+    {"empresa": "Sistemas.net", "cargo": "Desenvolvedor Front-end júnior", "lista de requisitos": [
+        "HTML5", "CSS", "React", "Angular"
+    ]}
+]
+
+//console.log(vagas)
